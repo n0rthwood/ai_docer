f61ca064b69f01eb1a779dd8f2a287c3942d63d4
Bill@jcv1 <bill@joysort.com>
2022-10-26 22:25:46 +0800

    add setup script

---
+++ setup_env.sh
@@ -0,0 +1,12 @@
+wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda_11.8.0_520.61.05_linux.run
+sudo sh cuda_11.8.0_520.61.05_linux.run
+
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
+sudo apt-key add -
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
+sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update
+sudo apt-get install -y docker.io docker-compose  nvidia-docker2
+sudo pkill -SIGHUP dockerd
+sudo nvidia-container-cli --load-kmods info
