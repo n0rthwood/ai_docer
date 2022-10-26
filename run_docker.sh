58f6961133cb23a624767727dd8fcc73626906c2
tcs1.joysort.net <bill@joysort.com>
2022-10-26 21:50:16 +0800

    add build script

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo script located at: $SCRIPT_DIR
-docker run --rm -it --privileged --network host -v /opt/images/:/opt/images/ -v $SCRIPT_DIR/code/.torch:/root/.torch/  -v $SCRIPT_DIR/code/:/opt/code --runtime nvidia ai_docker /bin/bash
+docker run --rm -it --privileged --network host -v /opt/workspace/mmclassification:/opt/mmclassification -v /opt/workspace/imagedb/input/DatasetId_1692766_1666577221:/opt/imagedb/train -v $SCRIPT_DIR/code/.torch:/root/.torch/  -v $SCRIPT_DIR/code/:/opt/code --runtime nvidia registry.cn-beijing.aliyuncs.com/joysort/training:latest python /opt/mmclassification/tools/train.py /opt/mmclassification/configs/joycv/resnet18_8xb16_freshchestnut.py 
